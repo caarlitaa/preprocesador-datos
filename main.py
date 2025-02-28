4bc2a4710d757a9953cd518158bfdc7814dfffb4
caarlitaa <carla.vazquez4@udc.es>
2025-02-28 18:57:42 +0100

    archivo main

---
+++ main.py
@@ -0,0 +1,9 @@
+import menu # Importamos la función menu desde menu_funciones.py
+
+def main():
+    """Función principal para ejecutar la aplicación."""
+    menu.menu()  # Llamamos a menu() para iniciar la aplicación
+
+# Ejecutar la función principal
+if __name__ == "__main__":
+    main()
